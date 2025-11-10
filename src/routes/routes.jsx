fd00c91260feafe8f5f1c54ce4323f590a3be17b
abrar-hossain20 <abrar1224noyon@gmail.com>
2025-11-11 02:33:55 +0600

    css classes are added

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,7 +4,6 @@ import Home from "../pages/Home";
 import Signin from "../pages/Signin";
 import Signup from "../pages/Signup";
 import CreateEvent from "../pages/CreateEvent";
-import UpcomingEvents from "../pages/UpcomingEvents";
 import PrivateRoute from "../privateRoutes/PrivateRoute";
 
 export const router = createBrowserRouter([
@@ -15,10 +14,6 @@ export const router = createBrowserRouter([
       {
         path: "/",
         element: <Home></Home>,
-      },
-      {
-        path: "/upcoming-events",
-        element: <UpcomingEvents></UpcomingEvents>,
       },
       {
         path: "/create-event",
